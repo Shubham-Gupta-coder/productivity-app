cc1e6c1b2acc77accd457296538fc5c17cc5aadd
Shubham <shubhamguptakrj2008@gmail.com>
2022-12-10 15:10:18 +0530

    Video 4

--- screens/NewTask.js
+++ screens/NewTask.js
@@ -1,89 +1,106 @@
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert } from 'react-native'
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const NewTask = ({navigation}) => {
+const NewTask = ({ navigation }) => {
     const [text, setText] = useState("")
     const [time, setTime] = useState("")
     const [date, setDate] = useState("")
     const [month, setMonth] = useState("")
-    const [deadline, setDeadline] = useState("")
 
     const finalDate = () => {
+        let task = {
+            text: text,
+            time: time,
+            date: date,
+            month: month,
+        }
+        const storeData = async () => {
+            try {
+                await AsyncStorage.setItem(
+                    'task',
+                    JSON.stringify(task)
+                );
+                await navigation.navigate("Tasks")
+
+            } catch (error) {
+                // Error saving data
+            }
+        }
         if (time !== "" && date !== "" && month !== "" && text !== "") {
-            setDeadline(time + " " + date + " " + month)
-            navigation.navigate("Tasks")
+            storeData();
         }
         else {
             Alert.alert(
-      "No Date Set",
-      "PLease Enter Time, Date And month before Submitting ",
-      [
-        {
-          text: "Cancel",
-          onPress: () => console.log("Cancel Pressed"),
-          style: "cancel"
-        },
-        { text: "OK", onPress: () => console.log("OK Pressed") }
-      ]
-    );
+                "No Date Set",
+                "PLease Enter Time, Date And month before Submitting ",
+                [
+                    {
+                        text: "Cancel",
+                        onPress: () => console.log("Cancel Pressed"),
+                        style: "cancel"
+                    },
+                    { text: "OK", onPress: () => console.log("OK Pressed") }
+                ]
+            );
 
         }
         if (text == "") {
             Alert.alert(
-      "Task not Entered",
-      "Please enter the task before Sumbit",
-      [
-        {
-          text: "Cancel",
-          onPress: () => console.log("Cancel Pressed"),
-          style: "cancel"
-        },
-        { text: "OK", onPress: () => console.log("OK Pressed") }
-      ]
-    );
+                "Task not Entered",
+                "Please enter the task before Sumbit",
+                [
+                    {
+                        text: "Cancel",
+                        onPress: () => console.log("Cancel Pressed"),
+                        style: "cancel"
+                    },
+                    { text: "OK", onPress: () => console.log("OK Pressed") }
+                ]
+            );
 
         }
     }
-  return (
-    <View>
-          <Text style={{ fontSize: 30, margin: 20, padding: 20, borderColor: 'blue', textAlign: 'center', color: 'blue', fontWeight: 'bold', borderRadius: 20,borderWidth:2 }}>Add A New Task</Text>
-          <View style={{width:'80%', marginLeft:'10%', marginTop:10}}>
-              <Text style={{margin:10, fontSize:20}}>Enter Yout Task Here</Text>
-          <TextInput
-        style={{  padding:20, borderRadius:20, borderWidth:2, borderColor: 'black', }}
-        placeholder="Task Here"
-        onChangeText={newText => setText(newText)}
-        defaultValue={text}
-      /></View>
-          <View style={{width:'80%', marginLeft:'10%', marginTop:10}}>
-              <Text style={{margin:10, fontSize:20}}>Final Time and Date</Text>
-          <TextInput
-        style={{  padding:20, borderRadius:20, borderWidth:2, borderColor: 'black', }}
-        placeholder="Time ex: 12:00"
-        onChangeText={newTime => setTime(newTime)}
-        defaultValue={time}
-              />
-          <TextInput
-        style={{  padding:20, borderRadius:20, borderWidth:2, borderColor: 'black',marginVertical:20 }}
-        placeholder="Date ex: 31"
-        onChangeText={newTime => setDate(newTime)}
-        defaultValue={date}
-              />
-          <TextInput
-        style={{  padding:20, borderRadius:20, borderWidth:2, borderColor: 'black', }}
-        placeholder="Month ex: 12"
-        onChangeText={newTime => setMonth(newTime)}
-        defaultValue={month}
-              />
-          </View>
-          <TouchableOpacity onPress={()=> finalDate()} style={styles.btn}>
-                    <Text style={styles.txt}>Add Tasks</Text>
-                </TouchableOpacity>
-          
-    </View>
-  )
+    return (
+        <View>
+            <Text style={{ fontSize: 30, margin: 20, padding: 20, borderColor: 'blue', textAlign: 'center', color: 'blue', fontWeight: 'bold', borderRadius: 20, borderWidth: 2 }}>Add A New Task</Text>
+            <View style={{ width: '80%', marginLeft: '10%', marginTop: 10 }}>
+                <Text style={{ margin: 10, fontSize: 20 }}>Enter Yout Task Here</Text>
+                <TextInput
+                    style={{ padding: 20, borderRadius: 20, borderWidth: 2, borderColor: 'black', }}
+                    placeholder="Task Here"
+                    onChangeText={newText => setText(newText)}
+                    defaultValue={text}
+                /></View>
+            <View style={{ width: '80%', marginLeft: '10%', marginTop: 10 }}>
+                <Text style={{ margin: 10, fontSize: 20 }}>Final Time and Date</Text>
+                <TextInput
+                    style={{ padding: 20, borderRadius: 20, borderWidth: 2, borderColor: 'black', }}
+                    placeholder="Time ex: 12:00"
+                    onChangeText={newTime => setTime(newTime)}
+                    defaultValue={time}
+                />
+                <TextInput
+                    style={{ padding: 20, borderRadius: 20, borderWidth: 2, borderColor: 'black', marginVertical: 20 }}
+                    placeholder="Date ex: 31"
+                    onChangeText={newTime => setDate(newTime)}
+                    defaultValue={date}
+                />
+                <TextInput
+                    style={{ padding: 20, borderRadius: 20, borderWidth: 2, borderColor: 'black', }}
+                    placeholder="Month ex: 12"
+                    onChangeText={newTime => setMonth(newTime)}
+                    defaultValue={month}
+                />
+            </View>
+            <TouchableOpacity onPress={() => finalDate()} style={styles.btn}>
+                <Text style={styles.txt}>Add Tasks</Text>
+            </TouchableOpacity>
+
+        </View>
+    )
 }
 
 export default NewTask
@@ -95,11 +112,11 @@ const styles = StyleSheet.create({
         padding: 20,
         borderRadius: 20,
         marginLeft: '10%',
-        marginTop:10
+        marginTop: 10
     },
     txt: {
         textAlign: 'center',
         fontSize: 20,
-        color:'white'
+        color: 'white'
     }
 })
--- screens/Tasks.js
+++ screens/Tasks.js
@@ -1,28 +1,75 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, ScrollView, Image, View, TouchableOpacity } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Navbar from '../components/Navbar'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const Tasks = ({navigation}) => {
+
+const Tasks = ({ navigation }) => {
+    const [val, setval] = useState({})
+    const [width, setwidth] = useState("0")
+    let _retrieveData = async () => {
+        try {
+            const value = await AsyncStorage.getItem('task');
+            if (value !== null) {
+                // We have data!!
+                setval(JSON.parse(value));
+                setwidth("90")
+                console.log(val)
+            }
+            else {
+                // No data
+                setval({});
+                setwidth("0")
+                console.log(val)
+
+            }
+        } catch (error) {
+            // Error retrieving data
+        }
+    };
+    let removeItem = async () => {
+        try {
+            await AsyncStorage.removeItem('task');
+            await _retrieveData()
+        } catch (error) {
+            // Error retrieving data
+        }
+    };
+    setInterval(() => {
+        _retrieveData()
+    }, 2000000)
+
     return (
-      <View style={styles.main}>
-    <View style={styles.container}>
+        <View style={styles.main}>
+            <View style={styles.container}>
                 <Text style={{ fontSize: 25, textAlign: 'center', marginTop: 20, fontWeight: 'blod', }}>Complete or Add Your Tasks</Text>
-                <TouchableOpacity onPress={()=> navigation.navigate("newtask")} style={styles.btn}>
+                <ScrollView style={{ width: `${width}%`, margin: '5%', }}>
+                    <View style={{ width: '100%', padding: 20, backgroundColor: '#3030ff', borderRadius: 10, flex: 1, justifyContent: 'space-around', flexDirection: 'row', alignItems: 'center' }}>
+                        <View>
+                            <Text style={{ fontSize: 30, color: 'white', fontWeight: 'bold' }}>{val.text} </Text>
+                            <Text style={{ color: 'white', marginTop: 10 }}>Due Date: {val.time} hrs {val.date}/{val.month}</Text>
+                        </View>
+                        <TouchableOpacity onPress={() => removeItem()}>
+                            <Image source={require('../assets/tick-symbol-icon.png')} style={{ width: 40, height: 40 }} />
+                        </TouchableOpacity>
+                    </View>
+                </ScrollView>
+                <TouchableOpacity onPress={() => navigation.navigate("newtask")} style={styles.btn}>
                     <Text style={styles.txt}>Add Tasks</Text>
                 </TouchableOpacity>
             </View>
-            <Navbar/>
+            <Navbar />
         </View>
-  )
+    )
 }
 
 export default Tasks
 
 const styles = StyleSheet.create({
     container: {
-        width:'100%',
+        width: '100%',
         flex: 1,
         height: '80%',
         alignItems: 'center',
@@ -32,17 +79,17 @@ const styles = StyleSheet.create({
     main: {
         flex: 1,
         justifyContent: 'center',
-        width:'100%'
+        width: '100%'
     },
     btn: {
         width: '80%',
         backgroundColor: 'blue',
         padding: 20,
-        borderRadius:20
+        borderRadius: 20
     },
     txt: {
         textAlign: 'center',
         fontSize: 20,
-        color:'white'
+        color: 'white'
     }
 })
